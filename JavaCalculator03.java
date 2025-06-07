140f7c126e9707764f62c8c865f3f54b764ffb95
Ken Fogel <omniprof@users.noreply.github.com>
2025-06-07 16:00:02 -0400

    Update JavaCalculator03.java
    Made changes required by Java 25

--- JavaCalculator03.java
+++ JavaCalculator03.java
@@ -1,32 +1,31 @@
-// Runs with java --enable-preview JavaCalculator03.java
-// using Java 23
-
-import static java.io.IO.*;
+// Runs with java JavaCalculator03.java
+// using Java 25
 
 public class JavaCalculator03 {
     
-    void main() {
-        var loan = inputData();
-        var result = processData(loan);
-        outputResult(result);
-    }
+   void main() {
+      var loan = inputData();
+      var result = processData(loan);
+      outputResult(result);
+   }
 
-    private LoanRecord inputData() {
-        var loan = Double.parseDouble(readln("Loan >> "));
-        var interest = Double.parseDouble(readln("Interest >> "));
-        var term = Double.parseDouble(readln("Term >> "));
-        return new LoanRecord(loan, interest, term);
-    }
+   private LoanRecord inputData() {
+      var loan = Double.parseDouble(IO.readln("Loan >> "));
+      var interest = Double.parseDouble(IO.readln("Interest >> "));
+      var term = Double.parseDouble(IO.readln("Term >> "));
+      return new LoanRecord(loan, interest, term);
+   }
 
-    private double processData(LoanRecord loan) {
-        double tempInterest = loan.interest() / 12.0;
-        double result = loan.loan() * (tempInterest / (1.0 - Math.pow((1.0 + tempInterest), -loan.term())));
-        return result;
-    }
+   private double processData(LoanRecord loan) {
+      double tempInterest = loan.interest() / 12.0;
+      double result = loan.loan() * (tempInterest /
+              (1.0 - Math.pow((1.0 + tempInterest), -loan.term())));
+      return result;
+   }
 
-    private void outputResult(double result) {
-        println("Monthly Payment >> " + String.format("%.2f", result));
-    }
+   private void outputResult(double result) {
+      IO.println("Monthly Payment >> " + String.format("%.2f", result));
+   }
 }
 
 record LoanRecord(double loan, double interest, double term) {}
