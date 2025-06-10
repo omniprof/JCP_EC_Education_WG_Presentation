707dceef59e928659f670e8451b9f87cdff4a675
Ken Fogel <omniprof@users.noreply.github.com>
2025-06-10 11:51:23 -0400

    Update JavaCalculator02.java

--- JavaCalculator02.java
+++ JavaCalculator02.java
@@ -2,9 +2,9 @@
 // using Java 25
 
 void main() {
-    var loan = Double.parseDouble(readln("Loan: "));
-    var interest = Double.parseDouble(readln("Interest: "));
-    var term = Double.parseDouble(readln("Term: "));
+    var loan = Double.parseDouble(IO.readln("Loan: "));
+    var interest = Double.parseDouble(IO.readln("Interest: "));
+    var term = Double.parseDouble(IO.readln("Term: "));
     var tempInterest = interest / 12.0;
     var result = loan * 
 		(tempInterest / (1.0 - Math.pow((1.0 + tempInterest), -term)));
